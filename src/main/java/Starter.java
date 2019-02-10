c0f704de8dcfcb74a4ecc92d59f2c05447fcd972
Artem Batalov <artem.batalov.prof@gmail.com>
2019-02-10 23:43:11 +0300

    + config experiment

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,10 +1,21 @@
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
 import services.MessageRouterVerticle;
 import services.RestServerVerticle;
 import services.WebSocketServerVerticle;
 
 public class Starter {
     public static void main(String[] args) {
+        ConfigStoreOptions store = new ConfigStoreOptions()
+            .setType("file")
+            .setFormat("yaml")
+            .setConfig(new JsonObject()
+                .put("path", "application.yml")
+            );
+        ConfigRetriever retriever = ConfigRetriever.create(Vertx.vertx(), new ConfigRetrieverOptions().addStore(store));
         deploy(Vertx.vertx());
     }
 

83d8ac0e0151daf73cb08eb3f0bf548cab0fa94a
Artem Batalov <artem.batalov.prof@gmail.com>
2019-05-17 22:02:44 +0300

    + heroku config

--- src/main/java/io/cell/Starter.java
+++ src/main/java/io/cell/Starter.java
@@ -3,19 +3,10 @@ package io.cell;
 import io.cell.services.MessageRouterVerticle;
 import io.cell.services.RestServerVerticle;
 import io.cell.services.WebSocketServerVerticle;
-import io.vertx.config.ConfigRetriever;
-import io.vertx.config.ConfigRetrieverOptions;
-import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
 
 public class Starter {
     public static void main(String[] args) {
-        ConfigStoreOptions store = new ConfigStoreOptions()
-            .setType("file")
-            .setFormat("json")
-            .setConfig(new JsonObject().put("path", "config.json"));
-        ConfigRetriever retriever = ConfigRetriever.create(Vertx.vertx(), new ConfigRetrieverOptions().addStore(store));
         deploy(Vertx.vertx());
     }
 
